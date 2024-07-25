a9c25789e489c0aac3098af4731436a67f599803
JvCas10 <jcastellanosp3@miumg.edu.gt>
2024-07-24 19:02:38 -0600

    holeeee

--- ProyectoAS2TaquillaCine/ProyectoAS2TaquillaCine/FormsAdmin/AgregarPeliculas.Designer.cs
+++ ProyectoAS2TaquillaCine/ProyectoAS2TaquillaCine/FormsAdmin/AgregarPeliculas.Designer.cs
@@ -49,6 +49,7 @@ namespace ProyectoAS2TaquillaCine.FormsAdmin
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -221,11 +222,22 @@ namespace ProyectoAS2TaquillaCine.FormsAdmin
             this.button4.Text = "GuardER";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(139, 298);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(227, 35);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Guardar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // AgregarPeliculas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 345);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
@@ -275,5 +287,6 @@ namespace ProyectoAS2TaquillaCine.FormsAdmin
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
