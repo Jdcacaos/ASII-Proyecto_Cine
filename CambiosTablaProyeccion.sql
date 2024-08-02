9cf758ed8cd1e00b176878164222b2a0384a3bcb
jlopezj555 <jlopezj24@miumg.edu.gt>
2024-08-01 18:05:04 -0600

    parte 2 Joel

--- CambiosTablaProyeccion.sql
+++
@@ -1,7 +0,0 @@
--- Modifiacion tbl_proyeccion
-use cine;
-
-ALTER TABLE `tbl_proyeccion`
-ADD COLUMN `Precio_Nino` decimal(10,2) NOT NULL,
-ADD COLUMN `Precio_Adulto` decimal(10,2) NOT NULL,
-ADD COLUMN `Precio_3ra_Edad` decimal(10,2) NOT NULL;
